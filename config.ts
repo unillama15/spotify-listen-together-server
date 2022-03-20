cb65c7e8211ec275579bd903a544b3254b00b0d9
Tree <75056118+unillama15@users.noreply.github.com>
2022-03-20 17:46:14 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 
 export default {
-  hostPassword: process.env.HOST_PASSWORD || "1234",
+  hostPassword: process.env.HOST_PASSWORD || "dddd_42069",
   maxDelay: parseInt(process.env.MAX_DELAY!) || 5000,
   clientVersionRequirements: "0.4.x",
   clientRecommendedVersion: "0.4.3"
